a202a8b6e0126d5dfa6795c9985d61bc4b16fcb7
ggrievous <49920741+ggrievous@users.noreply.github.com>
2019-04-24 23:52:25 +0530

    Update and rename app_chrome.py to app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+from multiprocessing.dummy import Pool
+from queue import Queue
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
@@ -13,17 +15,14 @@ options.headless = True
 chrome_driver_path = r"C:\Users\NH\PycharmProjects\SeleniumTest\drivers\chromedriver.exe"
 base_url = "https://www.8muses.com"
 
-
 def fetch_image_url(url,filename,download_location):
     driver = webdriver.Chrome(chrome_driver_path, chrome_options=options)
     driver.get(url)
     page = driver.page_source
     soup = BeautifulSoup(page,"lxml")
     image_url = "http:"+soup.find("img",{"class":"image"})['src']
-    driver.close()
     download_image(image_url,filename,download_location)
 
-    
 def download_image(image_url,filename,download_location):
     r = requests.get(image_url,stream=True, headers={'User-agent': 'Mozilla/5.0'})
     if r.status_code == 200:
@@ -50,17 +49,22 @@ if __name__=="__main__":
     print("Finding comic's pages")
     images = soup.find_all("a",{"class":"c-tile t-hover"})
     page_urls = []
+    pages = []
     threads = []
     for image in images:
         page_urls.append(base_url + image['href'])
     print("Found {} pages".format(len(page_urls)))
-    '''for i in range(len(page_urls)):
-        thread = Thread(target=fetch_image_url,args=(page_urls[i],i,download_location,))
-        threads.append(thread)
-    for thread in threads:
-        thread.start()
-    for thread in threads:
-        thread.join()'''
-    for i in range(len(page_urls)):
-        fetch_image_url(page_urls[i],i,download_location)
+    for i in  range(len(page_urls)):
+            pages.append((page_urls[i],i,download_location))
+    p = Pool(3)  # 3 threads in the pool
+    p.starmap(fetch_image_url,pages)
+    p.close()
+    p.join()
     driver.quit()
+    print ("DONE")
+
+
+
+
+
+
